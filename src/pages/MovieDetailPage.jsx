e72f4ccb7137086f5c3b6af17b33f65088a47d68
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-18 15:46:31 +0100

    fix: correzione in aula con Tizioano aggiungo nuova struttura chiamata Axios per dettaglio film con recensioni e rating

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,38 +1,68 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import MovieCard from "../components/MovieCard";
 import ReviewCard from "../components/ReviewCard";
+import Rating from "../components/Rating";
 
 export default function MovieDetailPage() {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
-  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3000/movies/${id}`)
-      .then((response) => {
-        setMovie(response.data.result[0]);
-        setReviews(response.data.result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    fetchMovie();
   }, []);
 
-  if (!movie) return <p>Caricamento...</p>;
+  function fetchMovie() {
+    axios.get(`http://localhost:3000/movies/${id}`).then((response) => {
+      const movie = response.data.result;
+
+      let voteSum = 0;
+      movie.reviews.forEach((review) => {
+        voteSum += review.vote;
+      });
+
+      const averageVote = Math.ceil(voteSum / movie.reviews.length);
+      movie.average_vote = averageVote;
+
+      setMovie(movie);
+    });
+  }
+
+  if (!movie) return <h1>Caricamento...</h1>;
+
   return (
     <div>
-      <h1>Dettaglio Film</h1>
-      <MovieCard
-        id={movie.id}
-        title={movie.title}
-        director={movie.director}
-        genre={movie.genre}
-        image={movie.image}
-      />
-      <h2>Recensioni</h2>
+      <h1>{movie.title}</h1>
+      <div className="row">
+        <div className="col-4">
+          <img
+            src={`http://localhost:3000/${movie.image}`}
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-8">
+          <div className="mb-2 border-bottom">
+            <strong>Regista:</strong>
+            <p>{movie.director}</p>
+          </div>
+          <div className="mb-2 border-bottom">
+            <strong>Genere:</strong>
+            <p>{movie.genre}</p>
+          </div>
+          <div className="mb-2 border-bottom">
+            <strong>Rating:</strong>
+            <Rating vote={movie.average_vote} maxVote={5} />
+          </div>
+          <div className="mb-2 border-bottom">
+            <strong>Abstract:</strong>
+            <p>{movie.abstract}</p>
+          </div>
+        </div>
+      </div>
+      <h2 className="mt-4">Recensioni</h2>
+      {movie.reviews.map((review) => (
+        <ReviewCard key={review.id} review={review} />
+      ))}
     </div>
   );
 }
