a31f67fdd78167f9cbb7f98bf3d138c748c8cfd9
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-18 04:45:23 +0100

    feat: aggiungo componente Layout con navbar e footer

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      {" "}
+      <nav>
+        {" "}
+        <a href="/">🎬 WebApp Movies Gabriela</a>{" "}
+      </nav>{" "}
+      <main> {children} </main>{" "}
+      <footer>
+        {" "}
+        <p>WebApp Movies Gabriela 2026</p>{" "}
+      </footer>{" "}
+    </div>
+  );
+}
