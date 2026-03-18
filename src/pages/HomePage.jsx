94f26bf9aa82b4f4426daf5289737dfeedbfa4eb
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-18 05:34:27 +0100

    feat: faccio chiamata Axios per lista film nella HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
+
 export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/movies").then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
   return (
     <div>
       <h1>Lista Film</h1>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          director={movie.director}
+          genre={movie.genre}
+          image={movie.image}
+        />
+      ))}
     </div>
   );
 }
