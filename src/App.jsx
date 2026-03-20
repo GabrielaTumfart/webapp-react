b56a8ca3833ab7d75f244a1496d5162e5f444f5e
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-20 17:07:16 +0100

    feat: aggiungo LoaderContextProvider e NotificationContextProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
+import { LoaderContextProvider } from "./contexts/LoaderContext";
+import { NotificationContextProvider } from "./contexts/NotificationContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies/:id" element={<MovieDetailPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <LoaderContextProvider>
+      <NotificationContextProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/movies/:id" element={<MovieDetailPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </NotificationContextProvider>
+    </LoaderContextProvider>
   );
 }
